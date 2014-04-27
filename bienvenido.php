cb3f50c3483a473050e574fb105ba0351d2f5e1d
DiFo92 <difo92@gmail.com>
2014-04-27 18:12:14 -0500

    Modulo administrador funcionando

---
+++ bienvenido.php
@@ -0,0 +1,67 @@
+<?php
+	session_start();
+	if(!isset($_SESSION["usuario"])){
+		header ("Location: index.php");
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Papeleria</title>
+	<script src="js/jquery-2.1.0.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/estilobanner.css">
+	<script type="text/javascript" src="js/bienvenido.js"></script>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<?php
+		if($_SESSION["usuario"]["id_rol"] == 1){
+			echo '
+				<!-- SCRIPTS PARA EL FUNCIONAMIENTO DE LOS INCLUDE CORRESPONDIENTES A ADMINISTRADOR -->
+					<script type="text/javascript" src="js/persona.js"></script>
+					<script type="text/javascript" src="js/producto.js"></script>
+					<script type="text/javascript" src="js/indicador.js"></script>
+				<!--  FIN  -->';
+		}
+	?>
+
+</head>
+<body>
+	<div class="row">
+		<div id ="banner" class="col-xs-12">
+			<img src="images/banner.jpg" id="img_banner">
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-7 col-xs-offset-3">
+			<?php
+				echo "<h3>Bienvenido: <b>".$_SESSION["usuario"]["nombre"]." ".$_SESSION["usuario"]["apellido"]."</b></h3>";
+			?>
+		</div>
+		<div class="col-xs-2">
+			<button id="salir" name="salir" class="btn btn-danger">Salir</button>
+		</div>
+	</div>
+	<div class="row" id="container_principal">
+		<section class="col-xs-10">
+			<article>
+				<div id="container">
+					Seleccione una opcion en el menu del lado derecho.
+				</div>
+			</article>
+		</section>
+		<aside id="menu" class="col-xs-2">
+			<?php
+				if($_SESSION["usuario"]["id_rol"] == 1){
+					include_once("view/menu_administrador.php");
+				}else if($_SESSION["usuario"]["id_rol"] == 2){
+					include_once("view/menu_empleado.php");
+				}
+			?>
+		</aside>
+	</div>
+	<footer id="pie">
+		<h5>Copiright Blancolino 2014</h5>
+	</footer>
+</body>
+</html>
---
+++ view/menu_empleado.php
@@ -0,0 +1,3 @@
+<ul class="nav nav-pills">
+	<li><a id="realizar_venta" href="#realizar_venta">Realizar Venta</a></li>
+</ul>
